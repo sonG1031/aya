7ca96911727bffb62cff450fc2aa6eda7b63c918
sonG1031 <minsub1031@gmail.com>
2022-01-05 20:16:19 +0900

    add audio

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const cntText = document.querySelector("#cnt-num");
 const minsub = document.querySelector("img");
 const page = document.querySelector("body");
-
+const audio = new Audio();
 let cnt = 0;
+
+audio.src = "sounds/아야.mp3";
+cntText.innerHTML = `${cnt}`;
 
 function isMobile() {
     let user = navigator.userAgent;
@@ -22,6 +25,7 @@ function changeImgDown() {
     cnt += 1;
     minsub.src = "img/after.png";
     cntText.innerHTML = `+${cnt}`;
+    audio.play();
 }
 function changeImgUp() {
     minsub.src = "img/before.png";
@@ -33,6 +37,6 @@ if (isMobile()) {
 } else {
     page.addEventListener("mousedown", changeImgDown);
     page.addEventListener("mouseup", changeImgUp);
-    page.addEventListener("keypress", changeImgDown);
+    page.addEventListener("keydown", changeImgDown);
     page.addEventListener("keyup", changeImgUp);
 }
